c5b8a48275fee287c22a2459f6359084cb90e0ea
Anisur369 <anisurrahman57136@gmail.com>
2026-06-15 23:48:42 +0600

    NavBar footer Logo authOption dbConnect

---
+++ src/components/buttons/NavLink.jsx
@@ -0,0 +1,13 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink=({href, children})=>{
+    const path = usePathname();
+    console.log(path)
+    return (
+        <Link className={`${path==href && "text-primary"} font-medium`} href={href}>{children}</Link>
+    )
+}
+
+export default NavLink;
---
+++ src/lib/authOptions.jsx
@@ -0,0 +1,74 @@
+import bcrypt from "bcrypt";
+import { dbConnect } from "@/lib/dbConnect";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions = {
+providers: [
+  CredentialsProvider({
+    name: 'Credentials',
+
+    credentials: {
+      email: { label: "Email", type: "email", placeholder: "Enter your email" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      const { email, password } = credentials;
+      const user = await dbConnect("users").findOne({ email });
+      if (!user) {
+        return null;
+      }
+      const isPasswordOk = await bcrypt.compare(password, user.password);
+      if (!isPasswordOk) {
+        return null;
+      }
+      return user;
+    }
+  }),
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  }),
+  GitHubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET
+  }),
+],
+callbacks: {
+  async signIn({ user, account, profile, email, credentials }) {
+    const payload = { ...user, email: user.email, provider: account.provider, providerId: account.providerId, role: "user" };
+
+    if(!user?.email) {
+      return false;
+    }
+    const existingUser = await dbConnect("users").findOne({ email: user.email });
+
+    if (!existingUser) {
+      await dbConnect("users").insertOne(payload);
+      return true
+    }
+
+    return true
+  },
+  // async redirect({ url, baseUrl }) {
+  //   return baseUrl
+  // },
+  async session({ session, token, user }) {
+    if (token) {
+      session.email = token.email;
+      session.role = token.role;
+    }
+    return session
+  },
+  async jwt({ token, user, account, profile, isNewUser }) {
+    if(user) {
+      token.email = user.email;
+      token.role = user.role;
+    }
+    return token
+  }
+}
+}
+
+export default authOptions;
